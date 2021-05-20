04ad846268fb08d586d56a2fced2d8411057cad7
edcod3 <edwinster99@gmail.com>
2021-05-20 11:21:39 +0200

    Added error log

--- nirh.py
+++ nirh.py
@@ -83,6 +83,7 @@ def doExtract(rar_location, temp_dir, verbose):
     except patoolib.util.PatoolError as err:
         printError("Couldn't extract rar archive...")
         print(err)
+        writeFind(err, "error.log")
         return False
 
 
